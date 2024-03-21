b88e4c264b3a22c12bb39cfb7c1decf42ea00ed5
Emmanuel M <emmanuelmmontesinos@gmail.com>
2024-03-21 18:48:33 +0100

    agregado uninstall

---
+++ uninstall.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo Borrando archivos...
+
+del DowYot.bat
+del DowYotMp3.py
+del icono.ico
+del install.bat
+del Pipfile
+del Pipfile.lock
+del README.MD
+del image.png
+
+
+pip uninstall pytube
+pip uninstall moviepy
+
+echo Desistalado, puede cerrar el terminal y borrar la carpeta.
+
+REM Pausa para que el usuario tenga la oportunidad de leer el mensaje antes de cerrar la ventana
+pause
