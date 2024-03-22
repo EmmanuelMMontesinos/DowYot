f7d23f72c004fc51984906ac84b07bc30e2878c9
Emmanuel M <emmanuelmmontesinos@gmail.com>
2024-03-22 23:12:30 +0100

    install update

--- install.bat
+++ install.bat
@@ -1,26 +1,16 @@
 @echo off
 
-
-
-REM Verificar si Python ya está instalado
-
-REM Descargar e instalar Python desde la página oficial
 echo Descargando e instalando Python...
 curl -o python-installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe || call :Error "Error al descargar Python"
 python-installer.exe /quiet TargetDir=C:\Python || call :Error "Error al instalar Python"
 del python-installer.exe
 
-pip install pipenv
-REM Instalar dependencias usando Pipenv
-echo Instalando dependencias con Pipenv...
-pipenv install || call :Error "Error al instalar dependencias con Pipenv"
 echo instalando dependencias: Pytube, moviepy
 pip install pytube
 pip install moviepy
 pip install ttkbootstrap
-echo Pytube instalado
+echo Pytube, movipy instalados
+
 REM Notificar al usuario que la instalación ha terminado
 echo Instalacion completa.
-
-REM Pausa para que el usuario tenga la oportunidad de leer el mensaje antes de cerrar la ventana
 pause
