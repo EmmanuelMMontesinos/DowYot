8694f843be29af1008e38e05230743de85024ee2
Emmanuel M <emmanuelmmontesinos@gmail.com>
2024-03-23 15:24:35 +0100

    install Update

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 
 echo Descargando e instalando Python...
 curl -o python-installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe || call :Error "Error al descargar Python"
-python-installer.exe /quiet TargetDir=C:\Python || call :Error "Error al instalar Python"
+python-installer.exe /quiet TargetDir=C:\Python InstallAllUsers=1 PrependPath=1 || call :Error "Error al instalar Python"
 del python-installer.exe
 
 echo instalando dependencias: Pytube, moviepy
