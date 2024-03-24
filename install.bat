e07eea37756b1307600a64425f9f9d7bd5121069
Emmanuel M <emmanuelmmontesinos@gmail.com>
2024-03-24 12:04:33 +0100

    install Update

--- install.bat
+++ install.bat
@@ -5,10 +5,16 @@ curl -o python-installer.exe https://www.python.org/ftp/python/3.9.7/python-3.9.
 python-installer.exe /quiet TargetDir=C:\Python InstallAllUsers=1 PrependPath=1 || call :Error "Error al instalar Python"
 del python-installer.exe
 
-echo instalando dependencias: Pytube, moviepy
+echo -------------------------------------------
+echo Instalando Pip
+python -m ensurepip --upgrade
+echo -------------------------------------------
+
+echo Instalando dependencias: Pytube, moviepy
 pip install pytube
 pip install moviepy
 pip install ttkbootstrap
+echo -------------------------------------------
 echo Pytube, movipy instalados
 
 REM Notificar al usuario que la instalación ha terminado
